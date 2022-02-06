cc7a409afa11152b3febc185e7a32531efb7b0b5
toppyishappy <topper0091@gmail.com>
2022-02-06 08:58:37 +0700

    init purchase-order path

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import HomePage
+from core.views import HomePage, PurchaseOrder
 
 urlpatterns = [
     path('', HomePage.as_view()),
+    path('purchase-order', PurchaseOrder.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required, name='dispatch')
 class HomePage(View):
 
     def get(self, request):
         return render(request, 'homepage.html')
+
+
+@method_decorator(login_required, name='dispatch')
+class PurchaseOrder(View):
+
+    def get(self, request):
+        return render(request, 'core/purchase-order.html')
