ab0944b305aa38bc9afc2e4e7ae74e0bc63dd5da
toppyishappy <topper0091@gmail.com>
2022-02-06 10:33:29 +0700

    add customer model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    fullname = models.CharField(max_length=50)
+    tel = models.CharField(max_length=10)
+    address = models.TextField()
+    delivery_date = models.DateField()
+
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
