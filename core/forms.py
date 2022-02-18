007eaec8a0dc4b70a40b713f7642f446b564970c
toppyishappy <topper0091@gmail.com>
2022-02-18 17:45:05 +0700

    fix purchase order layout

--- core/forms.py
+++ core/forms.py
@@ -8,9 +8,11 @@ class PurchaseOrderForm(forms.Form):
     province = forms.CharField(max_length=50)
     district = forms.CharField(max_length=50)
     zone = forms.CharField(max_length=50)
-    address = forms.CharField(widget=forms.Textarea)
+    address = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'style': 'width: 471px;'}))
     date = forms.DateField(widget=forms.TextInput(attrs={'class':'form-control', 'type': 'date'}))
     due_date = forms.DateField(widget=forms.TextInput(attrs={'class':'form-control', 'type': 'date'}))
+
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'style': 'width: 471px;'}), required=False)
  
     # sender = forms.EmailField()
     # cc_myself = forms.BooleanField(required=False)
