b4d8d3526f55dbe11e40313173774087237070f8
toppyishappy <topper0091@gmail.com>
2022-02-18 19:34:32 +0700

    add order detail edit page

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from core.views import HomePage, PurchaseOrder, AdminManagement, Management, Summary, PurchaseOrderDetail, PurchaseOrderDetailEdit
+from core.views import HomePage, PurchaseOrder, AdminManagement, Management, Summary, PurchaseOrderDetail, PurchaseOrderEdit
 
 urlpatterns = [
     path('', HomePage.as_view()),
     path('purchase-order/', PurchaseOrder.as_view()),
     path('purchase-order/<int:id>', PurchaseOrderDetail.as_view()),
-    path('purchase-order/edit/<int:id>', PurchaseOrderDetailEdit.as_view()),
+    path('purchase-order/edit/<int:id>', PurchaseOrderEdit.as_view()),
     path('admin-management/', AdminManagement.as_view()),
     path('management/', Management.as_view()),
     path('summary/', Summary.as_view()),
