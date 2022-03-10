2ef898a4829d388514a0464b2afd0a9dcbdea05b
toppyishappy <topper0091@gmail.com>
2022-03-10 22:17:23 +0700

    update setting compressor

--- furniture_crm/settings.py
+++ furniture_crm/settings.py
@@ -10,6 +10,13 @@ INSTALLED_APPS += [
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
+COMPRESS_ENABLED = True
+COMPRESS_FILTERS = {
+        "css": [
+            'compressor.filters.css_default.CssAbsoluteFilter',  
+            'compressor.filters.cssmin.CSSMinFilter',
+        ]
+}
 
 try:
     from configs import *  # pylint: disable=W0401
