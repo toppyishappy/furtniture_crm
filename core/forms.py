d22070f5ac223cdedf1c7ecaa8c7801be3a306a6
rungruj <rujeng@hotmail.com>
2022-03-12 16:44:01 +0700

    uptd

--- core/forms.py
+++ core/forms.py
@@ -8,13 +8,6 @@ from core.models import ItemColor, ItemMaterial, ItemModel, ItemType, SaleOrder,
 from user.models import EmployeeSignature
 
 class PurchaseOrderForm(forms.Form):
-    GEEKS_CHOICES =(
-    ("1", "One"),
-    ("2", "Two"),
-    ("3", "Three"),
-    ("4", "Four"),
-    ("5", "Five"),
-    )
     fullname = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     tel = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control'}), 
                             validators=[RegexValidator('^0\d{9}$', message='กรุณาตรวจสอบเบอร์โทรศัพ')])
@@ -22,7 +15,7 @@ class PurchaseOrderForm(forms.Form):
     # district = forms.CharField(max_length=50)
     # zone = forms.CharField(max_length=50)
     work_place_id = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-select'}), choices=WorkLocation.get_choices())
-    delivery_address = forms.CharField(widget=forms.Textarea(attrs={'rows': 1, 'class': 'form-select'}))
+    delivery_address = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control'}))
     date = forms.DateField(widget=forms.TextInput(attrs={'class':'form-control', 'type': 'date', 'id': 'date'}))
     delivery_date = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'id': 'weeklyDatePicker'}))
     province = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'id': 'province'}))
@@ -41,11 +34,11 @@ class ItemForm(forms.Form):
     color_id = forms.ChoiceField(choices=ItemColor.get_choices(), widget=forms.Select(attrs={'class':'form-select'}))
     material_id = forms.ChoiceField(choices=ItemMaterial.get_choices(), widget=forms.Select(attrs={'class':'form-select'}))
     files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': 'form-control'}), required=False)
-    amount = forms.IntegerField(min_value=0,required=True, initial=0,widget=forms.NumberInput(attrs={'class':'form-control'})) 
+    amount = forms.IntegerField(min_value=1,required=True, initial=1,widget=forms.NumberInput(attrs={'class':'form-control'})) 
     price = forms.DecimalField(min_value=0,required=True, initial=0,decimal_places=2,widget=forms.NumberInput(attrs={'class':'form-control'})) 
 
 class SaleForm(forms.Form):
-    # signature_id = forms.ChoiceField(choices=EmployeeSignature.get_choices(), widget=forms.Select(attrs={'class':'form-select'})) 
+    signature_id = forms.ChoiceField(choices=EmployeeSignature.get_choices(), widget=forms.Select(attrs={'class':'form-select'})) 
     deposite_type = forms.ChoiceField(choices=SaleOrder.DEPOSITE_CHOICES, widget=forms.Select(attrs={'class':'form-select'})) 
     payment_method = forms.ChoiceField(choices=SaleOrder.PATMENT_CHOICES, widget=forms.Select(attrs={'class':'form-select'})) 
     # payment_method = forms.ChoiceField(choices=SaleOrder.PATMENT_CHOICES, widget=forms.Select(attrs={'class':'form-select'}))
